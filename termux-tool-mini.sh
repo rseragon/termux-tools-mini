464643ce581895c0ffdeff56ce6cd031aed83f8c
Rseragon007 <rseragon702@gmail.com>
2019-01-09 21:29:46 +0530

    Add files via upload

---
+++ termux-tool-mini.sh
@@ -0,0 +1,101 @@
+#! /bin/bash
+
+apt-get update
+apt-get upgrade
+pkg install nano
+pkg install figlet
+pkg install toilet
+pkg install python
+pkg install python2
+pkg install apache2
+pkg install curl
+pkg install curl -y
+pkg install sl
+pkg install nano
+pkg install git
+pkg install cmatrix
+pkg install  php
+pkg install ncurses-utils
+pkg install tar
+pkg install openssh
+pkg install busybox
+apt-get install nmap
+
+figlet all pkgs
+
+pkg list-install
+
+figlet ^^ all pkgs ^^
+
+echo "PRESS ENTER TO CONTINUE"
+
+read
+
+touch log.txt
+
+clear
+
+toilet -f mono12 -F metal TOOLS
+
+echo "WELCOME TO MY FIRST BASH FILE"
+
+echo "TYPE YOUR USERNAME [just like that]: "
+read name
+
+echo "YOUR USERNAME IS :- $name "
+
+echo " installed all needed DEPENDENCIES"
+
+echo " cloning GIT repos"
+
+figlet Github
+git clone https://github.com/thelinuxchoice/shellphish.git
+
+git clone https://github.com/thelinuxchoice/instainsane.git
+
+git clone https://github.com/Mebus/cupp.git
+
+git clone https://github.com/grafov/hulk.git
+
+git clone https://github.com/Gameye98/Lazymux
+
+echo "cloned REPOS"
+figlet PHISH
+figlet cupp
+figlet IG brute
+figlet hulk
+figlet LMUX
+
+echo "type enter to continue"
+
+read 
+
+sl
+
+pkg install tsu
+pkg install wget
+
+clear
+
+echo "delete chache process"
+echo "press yes to continue"
+
+read ans
+
+rm log.txt
+
+
+figlet thk u
+
+figlet enjoy
+cd
+
+figlet BYE
+
+
+echo "IF YOU FIND ANY ERRORS FEEL FREE TO ASK ME"
+
+
+
+toilet -f mono9 -F metal BY
+toilet -f mono12 -F gay RSE
