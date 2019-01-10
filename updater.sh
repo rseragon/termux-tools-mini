80b2dab917d03bf5d3237d20fa0fa57ac934e2cb
Rseragon007 <rseragon702@gmail.com>
2019-01-10 19:42:17 +0530

    Fully stable v1

--- updater.sh
+++ updater.sh
@@ -12,8 +12,7 @@ rm tmux-mini.sh
 
 figlet updateing
 
-
-wget https://github.com/rseragon007/termux-tools-mini/blob/master/tmux-mini.sh
+wget https://raw.githubusercontent.com/rseragon007/termux-tools-mini/master/tmux-mini.sh
 
 chmod +x tmux-mini.sh
 
