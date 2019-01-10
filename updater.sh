62f29e1fa0a218adc0890bd4d020e05f75910e41
Rseragon007 <rseragon702@gmail.com>
2019-01-10 10:59:51 +0530

    alpha script v0.01
    still in alpha needs testing

---
+++ updater.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+echo "Welcome to the"
+
+figlet updater
+
+echo "press enter to continue"
+
+read
+
+figlet updateing
+
+git clone https://github.com/rseragon007/termux-tools-mini.git
+
+rm termux-tool-mini.sh
+
+cd termux-tools-mini
+
+
+mv termux-tool-mini.sh ..
+
+rm -rf termux-tools-mini
+
+clear
+
+figlet updated
+
+toilet -f mono12 -F gay BY
+toilet -f mono12 -F metal RSE007
