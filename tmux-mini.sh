83b2099e889cdc410116ed387ca8d792283a1ed1
Rseragon007 <rseragon702@gmail.com>
2019-01-16 11:25:09 +0530

    v2.0

--- tmux-mini.sh
+++ tmux-mini.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 
-
+yellow='\033[33m'
+red='\033[31m'
+green='\033[32m'
+white='\033[37m'
 
 echo
 touch log.txt
@@ -8,27 +11,28 @@ touch log.txt
 clear
 
 toilet -f mono9 -F metal TOOLS
+sleep 1
 toilet -f mono9 -F gay v1.0
 
-echo "WELCOME TO MY FIRST BASH FILE"
+echo -e "$red WELCOME TO MY FIRST BASH FILE"
 echo
-echo "TYPE YOUR REQ TOOL "
+echo -e "$green TYPE YOUR REQ TOOL "
 echo
-echo " [1] Shellphish "
+echo -e " $yellow [1] $red Shellphish "
 echo
-echo " [2] CUPP "
+echo -e " $yellow [2] $red CUPP "
 echo
-echo " [3] Instainsane"
+echo -e " $yellow [3] $red Instainsane "   
 echo
-echo " [4] Hulk-ddos "
+echo -e " $yellow [4] $red Hulk-ddos "
 echo
-echo " [5] Lazymux "
+echo -e " $yellow [5] $red Lazymux "
 echo
-echo " [6] Tool-X "
+echo -e " $yellow [6] $red Tool-X "
 echo
-echo " [7] NMAP "
+echo -e " $yellow [7] $red NMAP "
 echo
-echo "MORE COMING SOON"
+echo -e "$green MORE COMING SOON"
 echo
 echo
 read -p 'TOOL NO.>' tool
@@ -41,6 +45,7 @@ then
 figlet Github
 git clone https://github.com/thelinuxchoice/shellphish.git
 mv shellphish ..
+figlet PHISH
 fi
 
 if [ $tool -eq 2 ]
@@ -48,6 +53,7 @@ then
 figlet Github
 git clone https://github.com/Mebus/cupp.git
 mv cupp ..
+figlet CUPP
 fi
 
 if [ $tool -eq 3 ]
@@ -55,6 +61,7 @@ then
 figlet Github
 git clone https://github.com/thelinuxchoice/instainsane.git
 mv instainsane ..
+figlet IG brute
 fi
 
 if [ $tool -eq 4 ]
@@ -62,6 +69,7 @@ then
 figlet Github
 git clone https://github.com/grafov/hulk.git
 mv hulk ..
+figlet Hulk
 fi
 
 if [ $tool -eq 5 ]
@@ -69,6 +77,7 @@ then
 figlet Github
 git clone https://github.com/Gameye98/Lazymux
 mv Lazymux ..
+figlet LMUX
 fi
 
 if [ $tool -eq 6 ]
@@ -76,9 +85,10 @@ then
 figlet Github
 git clone https://github.com/Rajkumrdusad/Tool-X.git
 mv Tool-X ..
+filet TOOL-X
 fi
 
-if [ $tool -eq 7]
+if [ $tool -eq 7 ]
 then 
 apt-get update
 apt install nmap
@@ -87,17 +97,15 @@ fi
 
 if [ $tool -ge 8 ]
 then
-
 figlet wrong 
-
 exit 
 fi
 
-
+sleep 1
 clear
 
 echo "delete chache process"
-echo "press yes to continue"
+echo -e "to contiue type $red yes"
 
 read ans
 
